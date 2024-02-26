c1e5282512abfad85aa39ccbf9509fda8579763f
fulaytar <fulaytar2405@gmail.com>
2024-02-27 01:00:19 +0200

    дороби .

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,51 +1,21 @@
-// Описаний у документації
-import iziToast from "izitoast";
-// Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+export function imageSearch(query) {
+    const baseUrl = 'https://pixabay.com/api/';
+    const KeyApi = '42512842-e518c28c0b42a0fb4c46a85d3'
+    
+    const queryParams = new URLSearchParams({
+        key: KeyApi,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page:30,
+    })
 
-const form = document.querySelector('#form');
-const loader = document.querySelector('div');
-const gallery = document.querySelector('.gallery');
-
-const KEY = '42512842-e518c28c0b42a0fb4c46a85d3';
-const BASE_URL = 'https://pixabay.com/api/';
-let QUERY = '';
-const IMAGE_TYPE = 'photo';
-const ORIENTATION = 'horizontal'; 
-const SAFESEARCH = true;
-
-
-
-
-form.addEventListener('submit', startSearch);
-
-function startSearch(event) {
-    event.preventDefault();
-    const input = form.elements.input.value.trim();
-    if (input === "") {
-        return;
-    }
-    QUERY = input;
-    let LINK = `${BASE_URL}?key=${KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}`;
-    console.log(QUERY);
-    form.elements.input.value = "";
-    console.log(input);
-    fetch(LINK).then(response => {
+    return fetch(`${baseUrl}?${queryParams}`).then(response => {
         if (!response.ok) {
             throw new Error('Image error')
         }
         return response.json();
-    }).then(data => {
-        gallery.innerHTML = "";
-        const images = data.hits;
-        gallery.innerHTML = images.map(({ webformatURL }) => {
-            return `<img src="${webformatURL}" width="360" height="360">`
-        }).join('')
-    }).catch(error => {
-        iziToast.show({
-    title: 'Hey',
-    message: 'What would you like to add?'
-});
     })
-
 }
+/*  `<img src="${webformatURL}" width="360" height="360">` */
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,1 +1,67 @@
-console.log("render-ok")
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+import {imageSearch} from './pixabay-api'
+
+const form = document.querySelector('#form');
+const loader = document.querySelector('div');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener("submit", searchBtn);
+
+function searchBtn(event) {
+    event.preventDefault();
+    const query = form.elements.input.value.trim();
+    if (query === "") {
+        return;
+    }
+    //тут очищаю перед заповненням
+    gallery.innerHTML = '';
+    //очистка інпута
+    form.elements.input.value = "";
+    //пішла обробка
+    imageSearch(query).then(response => {
+        if(response.hits.length === 0){
+            iziToast.error({
+    title: 'Error',
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    position: 'topRight',
+});
+        }
+        
+        gallery.innerHTML= response.hits.map(element=> `<li class="item-gallery">
+          <a class="load-link" href="${element.largeImageURL}/${element.webformatURL}"
+            ><img
+            class="load-image"
+              src="${element.webformatURL}"
+              width="360"
+              height="360"
+              alt="${element.tags}"
+              title="${element.tags}"
+          /></a>
+          <ul class="load-list">
+            <li>
+              <h1>Likes</h1>
+              <p>${element.likes}</p>
+            </li>
+            <li>
+              <h1>Views</h1>
+              <p>${element.views}</p>
+            </li>
+            <li>
+              <h1>Comments</h1>
+              <p>${element.comments}</p>
+            </li>
+            <li>
+              <h1>Downloads</h1>
+              <p>${element.downloads}</p>
+            </li>
+          </ul>
+        </li>`).join('')
+        
+
+    }).catch(error => console.log(error))
+    
+}
