296c1f64582822bda65d535d69c2853895dad4da
fulaytar <fulaytar2405@gmail.com>
2024-02-27 15:09:37 +0200

    розкидай функції

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,8 +2,12 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-
-import {imageSearch} from './pixabay-api'
+//імпорт функці з другого файлу
+import { imageSearch } from './pixabay-api'
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('#form');
 const loader = document.querySelector('div');
@@ -12,15 +16,23 @@ const gallery = document.querySelector('.gallery');
 form.addEventListener("submit", searchBtn);
 
 function searchBtn(event) {
-    event.preventDefault();
-    const query = form.elements.input.value.trim();
-    if (query === "") {
+  event.preventDefault();
+  //зберігаю значення інпута
+  const query = form.elements.input.value.trim();
+  //очистка інпута
+  form.elements.input.value = "";
+  //перевірка інпута
+  if (query === "") {
         return;
     }
-    //тут очищаю перед заповненням
-    gallery.innerHTML = '';
-    //очистка інпута
-    form.elements.input.value = "";
+  //тут очищаю перед заповненням
+  gallery.innerHTML = '';
+  //включаю лоадер
+  loader.classList.add('spinner');
+
+  //відмальовую
+  setTimeout(() => {
+    loader.classList.remove('spinner')
     //пішла обробка
     imageSearch(query).then(response => {
         if(response.hits.length === 0){
@@ -28,40 +40,43 @@ function searchBtn(event) {
     title: 'Error',
     message: 'Sorry, there are no images matching your search query. Please try again!',
     position: 'topRight',
-});
+            });
         }
-        
-        gallery.innerHTML= response.hits.map(element=> `<li class="item-gallery">
-          <a class="load-link" href="${element.largeImageURL}/${element.webformatURL}"
+      gallery.innerHTML = response.hits.map(element => `<li class="item-gallery">
+          <a class="load-link" href="${element.largeImageURL}"
             ><img
             class="load-image"
               src="${element.webformatURL}"
               width="360"
-              height="360"
               alt="${element.tags}"
               title="${element.tags}"
           /></a>
           <ul class="load-list">
             <li>
-              <h1>Likes</h1>
+              <h2>Likes</h2>
               <p>${element.likes}</p>
             </li>
             <li>
-              <h1>Views</h1>
+              <h2>Views</h2>
               <p>${element.views}</p>
             </li>
             <li>
-              <h1>Comments</h1>
+              <h2>Comments</h2>
               <p>${element.comments}</p>
             </li>
             <li>
-              <h1>Downloads</h1>
+              <h2>Downloads</h2>
               <p>${element.downloads}</p>
             </li>
           </ul>
-        </li>`).join('')
-        
-
+        </li>`).join('');
+      const lightbox = new SimpleLightbox('.gallery>.item-gallery a', {
+        //* options */
+        captionsData: `alt`,
+        captionDelay: 250,
+      });
+      //метод рефреш
+      lightbox.refresh();
     }).catch(error => console.log(error))
-    
+  }, 500)
 }
